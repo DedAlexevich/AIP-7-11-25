faf1757def0a8020183be75867fd6082ab57eedf
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-07 12:53:23 +0300

    Изменил main, чтобы продемонстрировать add_row;

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ int* add_row(const int* a, size_t n, size_t m, int filler);
 
 int main()
 {
-  int* a = new int[5]{1,2,3,4,5};
-  int d = 15;
-  resize2(&a, 5, d, 3);
+  int* a = new int[10]{1,2,3,4,5,6,7,8,9,10};
+  int* b = add_row(a, 2, 5, 12);
 
-  for (size_t i = 0; i < d; ++i ) {
-    std::cout << a[i] << '\n';
+  for (size_t i = 0; i < 3; ++i) {
+    for (size_t j = 0; j < 5; ++j) {
+      std::cout << b[i*5 + j] <<'\t';
+    }
+    std::cout << '\n';
   }
   delete[] a;
+  delete[] b;
 }
 
 int* resize(const int* a, size_t k, size_t d, int filler)
