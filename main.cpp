df365e2a36c8e40f05d21ba9206149c43993f58b
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-07 13:16:38 +0300

    Добавлено транспонирование

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int* resize(const int* a, size_t k, size_t d, int filler);
 void resize2(int** a, size_t k, size_t d, int filler);
 int* add_row(const int* a, size_t n, size_t m, int filler);
+int* add_col(const int* a, size_t n, size_t m, int filler);
+int* transpon(const int* a, size_t n, size_t m);
 
 void show_matrix(const int* a, size_t n, size_t m)
 {
@@ -14,29 +16,21 @@ void show_matrix(const int* a, size_t n, size_t m)
   }
 }
 
-int* add_col(const int* a, size_t n, size_t m, int filler)
-{
-  int* narr = new int[n*(m+1)];
-  for (size_t i = 0; i < n; ++i) {
-    for (size_t j = 0; j <= m; ++j) {
-      narr[i*(m+1) + j] = a[i*m+j];
-    }
-    narr[i*(m+1) + m] = filler;
-  }
-  return narr;
-}
+
 
 int main()
 {
   int* a = new int[10]{1,2,3,4,5,6,7,8,9,10};
   int* b = add_row(a, 2, 5, 12);
   int* c = add_col(a,2,5,12);
-  show_matrix(b, 3,5);
-  show_matrix(c, 2,6);
+  int* d = transpon(a,2,5);
+  show_matrix(d, 5,2);
+  show_matrix(a, 2,5);
 
   delete[] a;
   delete[] b;
   delete[] c;
+  delete[] d;
 }
 
 int* resize(const int* a, size_t k, size_t d, int filler)
@@ -69,4 +63,27 @@ void resize2(int** a, size_t k, size_t d, int filler)
 int* add_row(const int* a, size_t n, size_t m, int filler)
 {
   return resize(a, n*m, (n+1)*m, filler);
+}
+
+int* add_col(const int* a, size_t n, size_t m, int filler)
+{
+  int* narr = new int[n*(m+1)];
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j <= m; ++j) {
+      narr[i*(m+1) + j] = a[i*m+j];
+    }
+    narr[i*(m+1) + m] = filler;
+  }
+  return narr;
+}
+
+int* transpon(const int* a, size_t n, size_t m)
+{
+  int* narr = new int[n*m];
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j) {
+      narr[j*n + i] = a[i*m + j];
+    }
+  }
+  return narr;
 }
