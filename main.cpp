f1a5e48bc3a1f737b9f5217ca7783c3ba8eb416e
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-07 12:13:32 +0300

    Add extend function and example

--- main.cpp
+++ main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
 
+int* extend(const int* a, size_t k, size_t d, int filler);
+
 int main()
-{}
+{
+  int a[] = {1,2,3,4,5};
+  int k = 3;
+  int* narr = extend(a, 5, k, 11);
+
+  for (size_t i = 0; i < k; ++i ) {
+    std::cout << narr[i] << '\n';
+  }
+  delete[] narr;
+}
+
+int* extend(const int* a, size_t k, size_t d, int filler)
+{
+  int* newArr = new int[d];
+  if (d > k) {
+    size_t i = 0;
+    for (; i < k; i++) {
+      newArr[i] = a[i];
+    }
+    for (; i < d; i++) {
+      newArr[i] = filler;
+    }
+  } else {
+    size_t i = 0;
+    for (; i < d; i++) {
+      newArr[i] = a[i];
+    }
+  }
+
+  return newArr;
+}
