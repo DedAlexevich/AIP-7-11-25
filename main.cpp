65dd5aa2571a2d97bd550da1ef16a2cd5a4d6e80
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-08 12:54:07 +0300

    Added func concat_rows and transpon

--- main.cpp
+++ main.cpp
@@ -4,33 +4,68 @@ int* resize(const int* a, size_t k, size_t d, int filler);
 void resize2(int** a, size_t k, size_t d, int filler);
 int* add_row(const int* a, size_t n, size_t m, int filler);
 int* add_col(const int* a, size_t n, size_t m, int filler);
+void add_col(int** a, size_t n, size_t m, int filler);
 int* transpon(const int* a, size_t n, size_t m);
 
-void show_matrix(const int* a, size_t n, size_t m)
+void show_matrix(int** a, size_t n, size_t m)
 {
   for (size_t i = 0; i < n; ++i) {
     for (size_t j = 0; j < m; ++j) {
-      std::cout << a[i*m + j] <<'\t';
+      std::cout << a[i][j] <<'\t';
     }
     std::cout << '\n';
   }
 }
 
+int** concat_rows(const int* const* lhs, size_t n1, size_t m1,
+const int* const* rhs, size_t n2, size_t m2)
+{
+  size_t newRows = std::max(n1,n2);
+  int** matr = new int*[newRows];
+  for (size_t i = 0; i < newRows; ++i) {
+    matr[i] = new int[m1+m2]{};
+  }
+
+  for (size_t i = 0; i < n1; ++i) {
+    for (size_t j = 0; j < m1; ++j) {
+      matr[i][j] = lhs[i][j];
+    }
+  }
+
+  for (size_t i = 0; i < n2; ++i) {
+    for (size_t j = 0; j < m2; ++j) {
+      if (i < n1) {
+        matr[i][j+m1] = rhs[i][j];
+      } else {
+        matr[i][j] = rhs[i][j];
+      }
+    }
+  }
+  show_matrix(matr, newRows, m1+m2);
+
+  return matr;
+}
 
 
 int main()
 {
-  int* a = new int[10]{1,2,3,4,5,6,7,8,9,10};
-  int* b = add_row(a, 2, 5, 12);
-  int* c = add_col(a,2,5,12);
-  int* d = transpon(a,2,5);
-  show_matrix(d, 5,2);
-  show_matrix(a, 2,5);
+  int** a = new int*[2];
+  int** b = new int*[2];
+
+
+  a[0] = new int[3]{1,2,3};
+  a[1] = new int[3]{6,7,8};
+
+  b[0] = new int[3]{4,5,6};
+  b[1] = new int[3]{9,10,11};
+
+  show_matrix(a, 2, 3);
+  show_matrix(b, 2, 3);
+
+  int** n = concat_rows(a, 2, 3, b, 2, 3);
 
   delete[] a;
   delete[] b;
-  delete[] c;
-  delete[] d;
 }
 
 int* resize(const int* a, size_t k, size_t d, int filler)
@@ -86,4 +121,12 @@ int* transpon(const int* a, size_t n, size_t m)
     }
   }
   return narr;
+}
+void add_col(int** a, size_t n, size_t m, int filler)
+{
+  for (size_t i = 0; i < n; ++i) {
+     int* nt = resize(*(a + i), m, m+1, filler);
+    delete[] a[i];
+    a[i] = nt;
+  }
 }
